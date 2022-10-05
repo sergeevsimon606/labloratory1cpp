a24425a1a06d4aae331d107a8a50a7243b6dcbf1
sergeevsimon606 <115061274+sergeevsimon606@users.noreply.github.com>
2022-10-05 08:18:39 +0300

    Add files via upload

---
+++ lab1#2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double t, z, x;
+    cout << "Input t: "; cin >> t;
+    cout << "Input z: "; cin >> z;
+    if (cos(t + z) > 0 && t != 0)
+    {
+        x = 4 * log(pow(cos(t + z), 3)) - z / t;
+        cout << "\nx=" << x << endl;
+    }
+    else cout << "Resheniya net!\n";
+    return 0;
+}
+
